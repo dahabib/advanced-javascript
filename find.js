d9c159bb95eec0d6ba32246d8b1928c01ecda8f8
Delwar Ahmed <delwar.habib@gmail.com>
2021-02-05 00:32:01 +0600

    practiced some methods

---
+++ find.js
@@ -0,0 +1,5 @@
+const numbers = [3, 4, 5, 6, 7, 8, 9];
+
+const isExist = numbers.find(x => x > 5);
+
+console.log(isExist);
---
+++ map.js
@@ -0,0 +1,21 @@
+const numbers = [3, 4, 5, 6, 7, 8, 9];
+
+// const output = [];
+
+// for(let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element * element;
+//     output.push(result);
+// }
+
+// const triple = element => element * element * element;
+// const double = x => x * 2;
+
+// const result = numbers.map(function(element, index, array){
+//     return element * element;
+// });
+
+const double = numbers.map(x => x * 2);
+
+
+console.log(double);
